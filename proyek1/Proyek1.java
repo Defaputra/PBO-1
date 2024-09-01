4600c144f72df314cb53c966a60dc1825300da29
Imam Adi Natadedfaputra <defaputra505@gmail.com>
2024-09-01 15:59:32 +0700

    Menambah file java

---
+++ proyek1/Proyek1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyek1;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Proyek1 {
+
+     public static void main(String[] args) {
+        String nama = "Defa Augista Montaine Dhanarto Putera";
+        String npm = "2320506028";
+        String alamat = "Sindanglangu Rt3/Rw7 Dayeuhluhur, Cilacap";
+        String noHp = "085600410652";
+
+        System.out.println("Nama : " + nama);
+        System.out.println("NPM : " + npm);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("No HP : " + noHp);
+    }
+}
